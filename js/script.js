661bf1d84b5157c492f248137d2c6d18554d62f1
Christian Sletten <35894685+csletten@users.noreply.github.com>
2018-02-06 14:43:16 +0100

    Test

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+
+
+
+*/
+
+class person {
+    constructor(health, weapon, speed, size, shape, fileName){
+        this.health = health;
+        this.weapon = weapon;
+        this.health = health;
+        this.health = health;
+        this.health = health;
+        this.health = health;
+        this.health = health;
+        
+    }
+}
