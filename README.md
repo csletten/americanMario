a6b83a3ccd6d86ace641f9150fad3ff2ba617b97
Christian Sletten <c.sletten99@gmail.com>
2018-02-08 23:08:08 +0100

    Lagt til klassen Weapon og nye ideer
    Ideene gjelder klasseinndeling for personene

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # americanMario
-Mario in 2D with an American theme. Lass.
+Mario in 2D with an American theme.
--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 /*
-<<<<<<< HEAD
-Test 2
 
-=======
-
-Test 4
->>>>>>> d698bb03a3e4350a2102551119420f9d316cb93f
-
+Likheter mellom personene:
+Løpe
+Stå stille
+Ulikheter:
+Hoppe
 */
 
-class person {
+class Person {
     constructor(health, weapon, speed, size, shape, fileName){
         this.health = health;
         this.weapon = weapon;
         this.speed = speed;
         this.size = size;
         this.shape = shape;
+        this.fileName = fileName;
+    }
+}
+
+class Weapon {
+    constructor(type, firerate, fileName){
+        this.type = type;
+        this.firerate = firerate;
         this.fileName = fileName;
     }
 }
